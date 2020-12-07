171e328c481aedf65120d524b33e606b56b16cfb
melhamdaoui811 <melhamdaoui811@gmail.com>
2020-12-07 20:38:11 +0100

    js function to add todo

---
+++ app.js
@@ -0,0 +1,33 @@
+// Selectors
+const input = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click",addTodo);
+
+//Fucntions
+ function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault(); 
+    // Todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // Create li
+    const todoLi=document.createElement("li");
+    todoLi.innerText="heey";
+    todoLi.classList.add("todo-item");
+    todoDiv.appendChild(todoLi);
+   //check mark Button
+   const completedButton =document.createElement("button");
+   completedButton.innerHTML ='<i class="fas fa-check"></i>';
+   completedButton.classList.add("complete-btn");
+   todoDiv.appendChild(completedButton);
+   //Trash Button
+   const trashButton =document.createElement("button");
+   trashButton.innerHTML ='<i class="fas fa-trash"></i>';
+   trashButton.classList.add("trash-btn");
+   todoDiv.appendChild(trashButton);
+   //append to List
+   todoList.appendChild(todoDiv);
+ }
